a4fa34a67ff11f763508ec5e290f21b67b458d40
Osorio, Aaron <osorio_arancibia.aaron_felipe@courrier.uqam.ca>
2024-11-07 17:18:58 -0500

    Correction du script js pour le labo 7

--- LABO7/static/js/script.js
+++ LABO7/static/js/script.js
@@ -1,121 +1,124 @@
-// valider format date AAAA-MM-JJ
+/**
+ * Voici le script revu et corriger vous permettant de bien réussir ce laboratoire
+ */
+
+//valider format de la date AAAA-MM-JJ
 function validerDate() {
-    let format = /[0-9]{4}-[0-1][0-9]-[0-3][0-9]/;
-    let date = document.getElementById("date").value;
+    let format= /[0-9]{4}-[0-1][0-9]-[0-3][0-9]/
+    let date = document.getElementById('date').value;
     let estValide = format.test(date);
     return estValide;
 }
-
-// tous les champs sont obligatoires
+// Tous les champs sont obligatoires
 function champsRemplis() {
     let estRempli = true;
     const ids = ["nom", "prenom", "date", "code", "genre"];
     for (var i=0; i < ids.length; i++) {
-        let champ = document.getElementById(ids[i]).value;
-        if (champ == null || champ.length < 1) {
-            estRempli = false;
+        let champs = document.getElementById(ids[i]).value;
+        if (champs == null || champs.length < 1){
+            estRemplis = false;
         }
     }
-    return estRempli;
+    return estRemplis;
 }
 
-// valider le champ genre
+// Valider le genre F ou M
 function validerGenre() {
-    let genre = document.getElementById("genre").value.toUpperCase();
-    return (genre == "M" || genre == "F");
+    let genre = document.getElementById('genre').value.toUpperCase();
+    return (genre == "M" || genre == "F" );
 }
 
-// valider le code permanent
-function validerCode() {
-    let code = document.getElementById("code").value;
+function validerCode () {
+    let code = document.getElementById('code').value;
 
     // verifier le format général du code permanent entré
     let format = /[A-Z]{4}[0-9]{8}/;
     let codeValide = format.test(code);
-    if (!codeValide) {
+    if(!codeValide) {
         return false;
     }
-
-    // verifier le 3 premier lettre du code permanent
-    let nom = document.getElementById("nom").value.substr(0,3).toUpperCase();
+    // verifier les 3 premieres lettre du code permanent
+    let nom = document.getElementById('nom').value.substr(0,3).toUpperCase();
     if (code.substr(0,3) !== nom) {
         return false;
     }
 
-    // verifier 4e lettre
-    let prenom = document.getElementById("prenom").value.toUpperCase();
+    //verifier la 4e lettre
+    let prenom = document.getElementById('prenom').value.toUpperCase();
     if (code[3] !== prenom[0]) {
         return false;
+
     }
 
-    let date = document.getElementById("date").value;
+    let date = document.getElementById('date').value;
 
-    // verifier que les 2 premiers chiffres correspondent au jour de naissance
+    // verifier que les 2 premiers chiffre correspondent au jour de naissance
     let jour = date.substr(8);
     if (code.substr(4,2) != jour) {
         return false;
     }
 
-    // verifier que les 2 chiffres suivants correspondent au mois de naissance
+
+    // verifier que les 2 chiffres suivant correspondent au mois de naissance
     let mois = parseInt(date.substr(5,2));
-    let genre = document.getElementById("genre").value.toUpperCase();
+    let genre = document.getElementById('genre').value.toUpperCase();
     let moisCode = parseInt(code.substr(6,2));
-    if ((genre === "F" && moisCode !== (mois + 50))
-        || (genre === "M" && moisCode !== mois)) {
+    if ((genre === "F" && moisCode !== (mois + 50)) || (genre === "M" && moisCode !== mois)){
         return false;
     }
 
-    // verifier que les 2 chiffres suivants correspondent à l'année de naissance
+    // verifier que les 2 chiffres suivant correspondent à l'année de naissance
     let annee = date.substr(2,2);
-    if (code.substr(8,2) !== annee) {
+    if(code.substr(8,2) !== annee) {
         return false;
     }
 
     return true;
 }
 
-// mettre ensembles tous les validations
-function valider() {
-    let errDate = document.getElementById("erreur-date");
+function valider(event){
+
+    let reussi = true;
+    let errDate = document.getElementById('erreur-date');
     let errChamps = document.getElementById("erreur-champs-vides");
     let errGenre = document.getElementById("erreur-genre");
     let errCode = document.getElementById("erreur-code");
-    let reussi = true;
 
-    if (!validerDate()) {
-        errDate.innerHTML = "La date est invalide !";
+    if(!validerDate()){
+        errDate.innerHTML = "La date est invalide!!!"
         reussi = false;
     } else {
         errDate.innerHTML = "";
     }
 
-    if (!champsRemplis()) {
-        errChamps.innerHTML = "1 ou plusieurs champs sont vides.";
+    if(!champsRemplis()){
+        errChamps.innerHTML = "1 ou plusieurs champs sont vides!!!"
         reussi = false;
     } else {
         errChamps.innerHTML = "";
     }
 
-    if (!validerGenre()) {
-        errGenre.innerHTML = "Le genre n'est pas valide."
+    if(!validerGenre()){
+        errGenre.innerHTML = "Le genre est invalide!!!"
         reussi = false;
     } else {
         errGenre.innerHTML = "";
     }
 
-    if (!validerCode()) {
-        errCode.innerHTML = "Le code permanent est invalide.";
+    if(!validerCode()) {
+        errCode.innerHTML = "Le code permanent est invalide!!!";
         reussi = false;
-    } else {
-        errCode.innerHTML = "";
+    }  else {
+        errCode.innerHTML = ""
     }
 
-    // si tous les validations réussissent, on submit. Sinon, on reste sur la page pour corriger les champs invalides.
+    // Si toutes les validations échouent, on empêche l'envoi du formulaire
     if (reussi) {
-        let form = document.getElementById("formulaire");
-        form.submit();
+        document.getElementById("formulaire").submit();
     } else {
-        let errGlobal = document.getElementById("erreur-global");
-        errGlobal.innerHTML = "1 ou plusieurs champs sont invalides.";
+        document.getElementById("erreur-global").innerHTML = "1 ou plusieurs champs sont invalides!!!";
     }
 }
+
+// Ajout de l'écouteur d'événement
+document.getElementById("formulaire").addEventListener("submit", valider);
